15493ac1bbb68ed6f8d2bc7fb95444a68850d856
Lucas <lucascandido.ferreira@gmail.com>
2024-01-10 15:20:19 -0300

    hospedando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,26 @@ export class AppComponent implements OnInit {
     this.dockItems = [
       {
           label: 'linkedln',
-          icon: "../assets/img/linkedin.png"
+          icon: "../assets/img/linkedin.png",
+          url: "https://www.linkedin.com/in/lcferreira98/"
       },
       {
           label: 'github',
-          icon: "../assets/img/github.png"
+          icon: "../assets/img/github.png",
+          url: "https://github.com/LFerreira0"
       },
-      {
-          label: 'traduçao',
-          icon: "../assets/img/global.png"
-      },
+      // {
+      //     label: 'traduçao',
+      //     icon: "../assets/img/global.png"
+      // },
       {
           label: 'curriculum',
-          icon: "../assets/img/curriculum-vitae.png"
+          icon: "../assets/img/curriculum-vitae.png",
+          url: "https://drive.google.com/file/d/1xwMd2kiWq9Eu7kSjwESFR75I-2hyFUIt/view?usp=sharing"
       }
   ];
   }
   title = 'Portifolio';
   dockItems!: MenuItem[];
+
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import { IRepository } from '../shared/interfaces/git-interface';
 import { GitService } from '../shared/services/git.service';
 
@@ -12,12 +11,11 @@ export class HeroComponent implements OnInit {
   inView!: string;
   isTestDivScrolledIntoView!: boolean;
   projetos!: IRepository[];
-  teste = {
-    element: ViewChild('about')
-  }
+  aboutView = false;
+  workView = false;
+  projectView = false;
   constructor(
-    private gitService: GitService,
-    private router: Router
+    private gitService: GitService,    
   ) { }
 
   ngOnInit(): void {
@@ -25,13 +23,48 @@ export class HeroComponent implements OnInit {
   }
 
   @ViewChild('about', {static: false}) private about!: ElementRef<HTMLDivElement>;
-  @ViewChild('trabalhos', {static: false})private trabalhos!: HTMLElement;
+  @ViewChild('trabalhos', {static: false}) private trabalhos!: ElementRef<HTMLDivElement>;
+  @ViewChild('projects', {static: false}) private projects!: ElementRef<HTMLDivElement>
   @ViewChild('conteudo') private content!: ElementRef<HTMLDivElement>;
 
-  @HostListener('window:scroll', ['$event'])
+  
+
+  @HostListener('wheel', ['$event'])
   isInViewPort(){
     if (this.about && this.trabalhos){
-     
+
+        if(this.about.nativeElement.getBoundingClientRect().top > 20 && this.about.nativeElement.getBoundingClientRect().top < 400)
+        {                    
+          document.getElementById("aboutBar")?.classList.add('focus')          
+          document.getElementById("aboutLabel")?.classList.add('focusLabel')     
+          this.aboutView = true;     
+        } else
+        {
+            document.getElementById("aboutBar")?.classList.remove('focus')          
+            document.getElementById("aboutLabel")?.classList.remove('focusLabel')
+            this.aboutView = false
+        }
+        
+        if (this.trabalhos.nativeElement.getBoundingClientRect().top > 60 && !this.aboutView){
+          console.log(this.trabalhos.nativeElement.getBoundingClientRect().top)
+          document.getElementById("workBar")?.classList.add('focus')          
+          document.getElementById("workLabel")?.classList.add('focusLabel')    
+          this.workView = true;
+        } 
+        else{
+          console.log(this.trabalhos.nativeElement.getBoundingClientRect().top)
+          document.getElementById("workBar")?.classList.remove('focus')          
+          document.getElementById("workLabel")?.classList.remove('focusLabel')    
+          this.workView = false;
+        }
+        
+        if(this.projects.nativeElement.getBoundingClientRect().top > 500 && !this.workView && !this.aboutView){
+          document.getElementById("projectsBar")?.classList.add('focus')          
+          document.getElementById("labelsBar")?.classList.add('focusLabel') 
+        }else{       
+          document.getElementById("projectsBar")?.classList.remove('focus')          
+          document.getElementById("labelsBar")?.classList.remove('focusLabel') 
+        }
     }
   }
   public getAllProjects(): void{
@@ -45,9 +78,5 @@ export class HeroComponent implements OnInit {
   public scroll(el:HTMLElement){
     this.content.nativeElement.scrollTo(0, el.offsetTop);
     window.scrollTo(0,0);
-
-    if(this.trabalhos.offsetTop >= 18){
-      
-    }
   }
 }
